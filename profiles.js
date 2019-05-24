f6b24e0e6795cdef416baace3701d94c4272f935
Danielo Rodriguez <rdanielo@gmail.com>
2019-05-24 15:22:50 +0200

    Updates

--- profiles.js
+++ profiles.js
@@ -1,8 +1,362 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"ncRp":[function(require,module,exports) {
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"ncRp":[function(require,module,exports) {
 var define;
-var e;!function(t,r){"object"==typeof exports&&"object"==typeof module?module.exports=r():"function"==typeof e&&e.amd?e("tasker-js-runner",[],r):"object"==typeof exports?exports["tasker-js-runner"]=r():t["tasker-js-runner"]=r()}("undefined"!=typeof self?self:this,function(){return function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=r(1),o=r(2);const s=()=>{return"development"!==n.a.global("TJS_ENV")?Promise.resolve():fetch(n.a.global("TJS_DEV_REMOTE")).then(e=>e.text()).then(e=>{n.a.readFile(n.a.global("TJS_LOCAL_PATH"))!==e&&(n.a.writeFile(n.a.global("TJS_LOCAL_PATH"),e),n.a.flash("script updated"),n.a.performTask("TJS:RunScript",n.a.local("priority"),JSON.stringify(n.a.locals)),n.a.exit())}).catch(e=>n.a.flash(e.message))};t.default=class{constructor(e){this.router=new o.a(e,n.a),s().then(()=>this.router.dispatch(n.a.locals).catch(e=>n.a.flash(e.message)).then(()=>n.a.exit()))}}},function(e,t,r){"use strict";var n=window;window.tasker=n,n.inspect=(e=>{const t=[];return JSON.stringify(e,function(e,r){if("object"==typeof r&&null!==r){if(-1!==t.indexOf(r))return;t.push(r)}return r})}),n.console={log(...e){n.flash(e.map(e=>"string"==typeof e?e:n.inspect(e)).join(" "))}},n.getParams=(()=>[n.local("par1"),n.local("par2")].map(e=>{let t;try{t=JSON.parse(e)}catch(r){t=e}return"undefined"===t?void 0:t}));const o=n.getParams()[0];n.locals=o||local_keys.reduce((e,t)=>{const r=t.slice(1);return e[r]=n.local(r),e},{}),t.a=n},function(e,t,r){"use strict";t.a=class{constructor(e,t){this.context=t,this.routes=e,this.routes.ui||(this.routes.ui={enter(){},exit(){}})}dispatch(e){return Promise.resolve().then(()=>{const t=e.callerdebug||e.caller2||e.caller1;var r=t.indexOf(":");const n=t.slice(0,r).split("=")[1]||"enter",o=t.slice(r+1);return(this.routes[o]||this.routes.ui)[n](e,this.context)})}}}])});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("tasker-js-runner", [], factory);
+	else if(typeof exports === 'object')
+		exports["tasker-js-runner"] = factory();
+	else
+		root["tasker-js-runner"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__tasker__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__router__ = __webpack_require__(2);
+
+
+
+const hotReload = () => {
+  const environment = __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].global('TJS_ENV');
+  if (environment !== 'development') return Promise.resolve();
+  return fetch(__WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].global('TJS_DEV_REMOTE')).then(res => res.text()).then(result => {
+    const existingFile = __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].readFile(__WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].global('TJS_LOCAL_PATH'));
+
+    if (existingFile !== result) {
+      __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].writeFile(__WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].global('TJS_LOCAL_PATH'), result);
+      __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].flash('script updated');
+      __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].performTask('TJS:RunScript', __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].local('priority'), JSON.stringify(__WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].locals));
+      __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].exit();
+    }
+  }).catch(err => __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].flash(err.message));
+};
+
+class TaskerJS {
+  constructor(routes) {
+    this.router = new __WEBPACK_IMPORTED_MODULE_1__router__["a" /* default */](routes, __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */]);
+    hotReload().then(() => this.router.dispatch(__WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].locals).catch(err => __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].flash(err.message)).then(() => __WEBPACK_IMPORTED_MODULE_0__tasker__["a" /* default */].exit()));
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["default"] = TaskerJS;
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* global local_keys */
+var tasker = window;
+window.tasker = tasker; // Injecting development functions
+
+tasker.inspect = target => {
+  const cache = [];
+  return JSON.stringify(target, function (key, value) {
+    if (typeof value === 'object' && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return;
+      } // Store value in our collection
+
+
+      cache.push(value);
+    }
+
+    return value;
+  });
+};
+
+tasker.console = {
+  log(...params) {
+    tasker.flash(params.map(param => typeof param === 'string' ? param : tasker.inspect(param)).join(' '));
+  }
+
+};
+
+tasker.getParams = () => {
+  return [tasker.local('par1'), tasker.local('par2')].map(rawParam => {
+    // Test if param is a json
+    let parsedParam;
+
+    try {
+      parsedParam = JSON.parse(rawParam); // will fail if param is not a JSON
+    } catch (err) {
+      parsedParam = rawParam;
+    }
+
+    return parsedParam === 'undefined' ? undefined : parsedParam;
+  });
+}; // Attempt to restore param from upstream
+
+
+const localsJson = tasker.getParams()[0];
+tasker.locals = localsJson || local_keys.reduce((acc, key) => {
+  const keyName = key.slice(1);
+  acc[keyName] = tasker.local(keyName);
+  return acc;
+}, {});
+/* harmony default export */ __webpack_exports__["a"] = (tasker);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Router {
+  constructor(routes, context) {
+    this.context = context;
+    this.routes = routes;
+
+    if (!this.routes.ui) {
+      this.routes.ui = {
+        enter() {},
+
+        exit() {}
+
+      };
+    }
+  }
+
+  dispatch(locals) {
+    return Promise.resolve().then(() => {
+      // Make route
+      const callerInfo = locals.callerdebug || locals.caller2 || locals.caller1;
+      var i = callerInfo.indexOf(':');
+      const caller = {
+        type: callerInfo.slice(0, i).split('=')[1] || 'enter',
+        route: callerInfo.slice(i + 1)
+      }; // Go to route
+
+      const route = this.routes[caller.route] || this.routes.ui;
+      return route[caller.type](locals, this.context);
+    });
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Router;
+
+
+/***/ })
+/******/ ]);
+});
 },{}],"pfd8":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.startTask=void 0;var t={enter:function(t,e){e.flash("Starting task");var a="TASK_".concat(t.par1,"_START");e.setGlobal(a,Date.now().toString())},exit:function(t,e){}};exports.startTask=t;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.startTask = void 0;
+var startTask = {
+  enter: function enter(locals, tasker) {
+    tasker.flash('Starting task');
+    var varName = "TASK_".concat(locals.par1, "_START");
+    tasker.setGlobal(varName, Date.now().toString());
+  },
+  exit: function exit(locals, tasker) {}
+};
+exports.startTask = startTask;
 },{}],"cPI2":[function(require,module,exports) {
-"use strict";var e=t(require("tasker-js-runner")),r=require("./modules/timeManager");function t(e){return e&&e.__esModule?e:{default:e}}new e.default({taskStart:r.startTask});
+"use strict";
+
+var _taskerJsRunner = _interopRequireDefault(require("tasker-js-runner"));
+
+var _timeManager = require("./modules/timeManager");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Construct Tasker JS and pass in mapping information as an Object
+new _taskerJsRunner.default({
+  // Profile name: module
+  'taskStart': _timeManager.startTask
+});
 },{"tasker-js-runner":"ncRp","./modules/timeManager":"pfd8"}]},{},["cPI2"], null)
--- task-workStop.js
+++ task-workStop.js
@@ -1,9 +1,239 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"8Lq7":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.readArr=exports.default=void 0;var r=function(r){var e=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{};try{return JSON.parse(r)}catch(t){return e}},e=r;exports.default=e;var t=function(e){return r(tk.global(e),[])};exports.readArr=t;
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"8Lq7":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.readArr = exports.default = void 0;
+
+var safeParse = function safeParse(str) {
+  var defaultVal = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    return defaultVal;
+  }
+};
+
+var _default = safeParse;
+exports.default = _default;
+
+var readArr = function readArr(name) {
+  return safeParse(tk.global(name), []);
+};
+
+exports.readArr = readArr;
 },{}],"T/DR":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var t=function(t){return function(){try{t.apply(void 0,arguments)}catch(e){tk.flashLong(e.toString())}}},e=t;exports.default=e;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var logErrs = function logErrs(fn) {
+  return function () {
+    try {
+      fn.apply(void 0, arguments);
+    } catch (error) {
+      tk.flashLong(error.toString());
+    }
+  };
+};
+
+var _default = logErrs;
+exports.default = _default;
 },{}],"rzbf":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.stopTask=exports.startTask=exports.pauseTask=exports.updateTask=exports.loadTask=void 0;var t=r(require("../util/safeParse")),e=r(require("../util/errLog"));function r(t){return t&&t.__esModule?t:{default:t}}var a=function(e,r){return(0,t.default)(tk.global(e),r)},s=function(t,e){return tk.setGlobal(t,JSON.stringify(e))},n=function(t){return a("TASK_".concat(t),{title:t,startedAt:null,pauses:[],stoppedAt:null})};exports.loadTask=n;var o=function(t){return function(e){return function(r){try{var a=n(r);return a[t]=e(a[t]),s("TASK_".concat(r),a),a}catch(o){tk.flash("Error updating task ".concat(r,":\n ").concat(o.toString))}}}};exports.updateTask=o;var u=o("pauses")(function(t){return t.push(Date.now()),t});exports.pauseTask=u;var p=o("startedAt")(function(){return Date.now()});exports.startTask=p;var i=o("stoppedAt")(function(){return Date.now()});exports.stopTask=i,window.pauseTask=(0,e.default)(u),window.startTask=(0,e.default)(p),window.stopTask=(0,e.default)(i);
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.stopTask = exports.startTask = exports.pauseTask = exports.updateTask = exports.loadTask = void 0;
+
+var _safeParse = _interopRequireDefault(require("../util/safeParse"));
+
+var _errLog = _interopRequireDefault(require("../util/errLog"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var readObj = function readObj(name, fallback) {
+  return (0, _safeParse.default)(tk.global(name), fallback);
+};
+
+var saveJson = function saveJson(name, value) {
+  return tk.setGlobal(name, JSON.stringify(value));
+};
+
+var loadTask = function loadTask(name) {
+  return readObj("TASK_".concat(name), {
+    title: name,
+    startedAt: null,
+    pauses: [],
+    stoppedAt: null
+  });
+};
+
+exports.loadTask = loadTask;
+
+var updateTask = function updateTask(field) {
+  return function (updater) {
+    return function (name) {
+      try {
+        var task = loadTask(name);
+        task[field] = updater(task[field]); // pass the current value for convenience 
+
+        saveJson("TASK_".concat(name), task);
+        return task;
+      } catch (error) {
+        tk.flash("Error updating task ".concat(name, ":\n ").concat(error.toString));
+      }
+    };
+  };
+};
+
+exports.updateTask = updateTask;
+var pauseTask = updateTask('pauses')(function (current) {
+  current.push(Date.now());
+  return current;
+});
+exports.pauseTask = pauseTask;
+var startTask = updateTask('startedAt')(function () {
+  return Date.now();
+});
+exports.startTask = startTask;
+var stopTask = updateTask('stoppedAt')(function () {
+  return Date.now();
+});
+exports.stopTask = stopTask;
+window.pauseTask = (0, _errLog.default)(pauseTask);
+window.startTask = (0, _errLog.default)(startTask);
+window.stopTask = (0, _errLog.default)(stopTask);
 },{"../util/safeParse":"8Lq7","../util/errLog":"T/DR"}],"8Zl5":[function(require,module,exports) {
-"use strict";var s=require("./tasks");(0,s.stopTask)("work"),tk.exit();
+"use strict";
+
+var _tasks = require("./tasks");
+
+(0, _tasks.stopTask)('work');
+tk.exit();
 },{"./tasks":"rzbf"}]},{},["8Zl5"], null)
