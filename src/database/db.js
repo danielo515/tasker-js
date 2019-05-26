11358f92c9ace01ffc26ba95f23b143fbfeef77e
Danielo Rodriguez <rdanielo@gmail.com>
2019-05-26 12:05:01 +0200

    chore: move the mockup for browser to a separate file

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,14 @@
 
 import TaskerAdapter from './taskerAdapter';
 import lowDb from 'lowdb';
-import { emptyTask } from '../emptyTask';
 import { fakeTasker } from './fakeTasker';
-
-let tasks;
 
 if (!window.tk) {
     // browser fake mode
-    const taskNames = ['work', 'eating', 'workout', 'programming'];
     fakeTasker();
-    tasks = [require('../../fixtures/fakeTask').running, ...taskNames.slice(1).map(emptyTask)];
 }
 
 export const db = lowDb(new TaskerAdapter(
     'Documents/tasks-db.json'
-    , { defaultValue: { tasks: tasks || [] } }
+    , { defaultValue: { tasks: [] } }
 ));
