3c12c95035a9e4795c8010b8e877102f97177f4f
Danielo Rodriguez <rdanielo@gmail.com>
2019-05-29 22:11:01 +0200

    feat: inline playPause

--- src/dashboard/components/PlayPause.js
+++ src/dashboard/components/PlayPause.js
@@ -3,12 +3,26 @@ import { TaskStatus } from '../../core/tasks';
 import IconButton from '@material-ui/core/IconButton';
 import PlayIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
+import PropTypes from 'prop-types';
 
-const PlayPause = (status, { onStart, onPause }) => 
-    <IconButton aria-label="Delete" color='primary'>
+
+const PlayPause = ({ status, onStart, onPause, inline }) => 
+    <IconButton aria-label="Delete" color='primary' style={inline ? {padding: 0}:{}}>
         {status === TaskStatus.RUNNING
             ? <PauseIcon onClick={onPause}></PauseIcon>
             : <PlayIcon onClick={onStart}></PlayIcon>}
     </IconButton>;
+
+PlayPause.propTypes={
+    status: PropTypes.string.isRequired,
+    onStart: PropTypes.func,
+    onPause: PropTypes.func,
+    inline: PropTypes.bool,
+};
+
+
+PlayPause.defaultProps = {
+    inline: true,
+};
 
 export default PlayPause;
